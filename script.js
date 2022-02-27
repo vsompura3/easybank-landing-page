3725d6c3cb978cf4aaf591f2a7ac1b4f33586692
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-02-28 04:02:47 +0530

    added inert polyfill

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const nav = document.getElementById('primary-navigation');
 const navToggle = document.querySelector('.navbar-menu-toggle');
 const backdrop = document.querySelector('.backdrop');
 
+nav.inert = true;
 navToggle.addEventListener('click', function () {
   const isOpen = nav.getAttribute('data-visible');
   if (isOpen === 'false') {
+    nav.inert = false;
     nav.setAttribute('data-visible', true);
     navToggle.setAttribute('aria-expanded', true);
   } else {
+    nav.inert = true;
     nav.setAttribute('data-visible', false);
     navToggle.setAttribute('aria-expanded', false);
   }
